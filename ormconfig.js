6481958b38f346b17019efb7f69ed680776fb8d6
Paulo Melo <peimelo@gmail.com>
2020-08-29 12:53:10 -0300

    Confirm typeorm

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,5 @@
+const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'dist/src';
+
 module.exports = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
@@ -5,9 +7,9 @@ module.exports = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  entities: ['dist/**/*.entity.js'],
-  migrations: ['dist/migrations/*.js'],
+  entities: [rootDir + '/**/*.entity{.js,.ts}'],
+  migrations: [rootDir + '/migrations/*{.js,.ts}'],
   cli: {
-    migrationsDir: 'src/migrations',
+    migrationsDir: rootDir + '/migrations',
   },
 };
